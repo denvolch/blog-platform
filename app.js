934edeb6e3e57b0c14a4f595e8900c115561bd93
denvolch <wolkov.web@yandex.ru>
2024-03-05 11:15:59 +0300

    refactoring code into modules then linking it

---
+++ app.js
@@ -0,0 +1,14 @@
+const config = require('./utils/config')
+const express = require('express')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blog')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const app = require('./app')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  Blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
